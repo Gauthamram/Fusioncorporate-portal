6ef5cb33980101557e680b0d4df5955f4979696b
Ram <Gauthamram@users.noreply.github.com>
2023-07-10 15:20:57 +1000

    Update npm packages and jquery versions

--- portal/resources/views/includes/head.blade.php
+++ portal/resources/views/includes/head.blade.php
@@ -8,13 +8,13 @@
 <script src="{{ asset('js/printer/sha-256.min.js')}}"></script>
 <script src="{{ asset('js/printer/qz-tray.js')}}"></script>
 <!-- jQuery Js Styles-->
-<!-- <script src="{{ asset('js/jquery/jquery-3.2.1.min.js') }}"></script> -->
+<script src="{{ asset('js/jquery/jquery-3.7.0.min.js') }}"></script>
  <!-- Data Table Js and CSS -->
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css"/>
 <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
 <!-- ui autocomplete -->
-<script src="{{ asset('js/jquery-ui-1.12.1.custom/jquery-ui.min.js') }}"></script>
-<link href="{{ asset('js/jquery-ui-1.12.1.custom/jquery-ui.min.css') }}" rel="stylesheet" />
+<script src="{{ asset('js/jquery-ui-1.13.2.custom/jquery-ui.min.js') }}"></script>
+<link href="{{ asset('js/jquery-ui-1.13.2.custom/jquery-ui.min.css') }}" rel="stylesheet" />
 <link href="{{ asset('css/main.min.css') }}" rel="stylesheet" />
 <!-- FontAwesome Styles-->
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
